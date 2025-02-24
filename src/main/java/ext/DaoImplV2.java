3fcd0a028ef1f441def9172a4047c2e42cd7113a
BHC <oussamabhc@192.168.1.14>
2025-02-24 22:16:19 +0000

    Injection des dépendances avec Spring (configuration XML)

--- src/main/java/ext/DaoImplV2.java
+++ src/main/java/ext/DaoImplV2.java
@@ -1,4 +1,11 @@
 package ext;
 
-public class DaoImplV2 {
+import dao.IDao;
+
+public class DaoImplV2 implements IDao {
+    public double getData() {
+        System.out.println("Version web service");
+        double t = 11;
+        return t;
+    }
 }
